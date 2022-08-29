ccbb2b11b30410b2f33262be8fe7a3d6f3c3a51c
Hunter McKay <hmckay2@huskers.unl.edu>
2022-08-29 00:52:58 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author:  Hunter McKay
+ * Date: 8/28/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Hunter McKay\n");
+
+  return 0;
+}
